fa5a4b8b299b2ebefe4f42e5674202feb17d7900
spitchers9 <shaun.pitchers1@gmail.com>
2018-05-31 10:15:12 +0100

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Shaun Pitchers on 31/05/2018.
+//  Copyright © 2018 Shaun Pitchers. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
